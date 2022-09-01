e36fda0ad832241f6679dcc4eea529c3723b3d24
NachoCheeseOfDoom <85528067+NachoCheeseOfDoom@users.noreply.github.com>
2022-09-01 17:08:18 -0500

    Added Dice

--- main.js
+++ main.js
@@ -3,9 +3,11 @@ let sumEl = document.getElementById("sum-el");
 let tirarDadoBtn = document.getElementById("tirarBtn");
 let dados = []
 
+const dice1El = document.getElementById("dice1");
+const dice2El = document.getElementById("dice2");
+
 
 tirarDadoBtn.addEventListener("click", function () {
-  console.log(tiradaRandom());
   tirarDados();
 });
 
@@ -27,7 +29,37 @@ function mostrarJuego() {
     dadosEl.innerHTML += dados[i] + " "
   }
   sumEl.textContent = "Sum: " + sum
+  if (dados[0] === 1) {
+    dice1El.innerHTML = `<img src="dices/dice1.png" alt="dice 1" />`
+  } else if (dados[0] === 2) {
+    dice1El.innerHTML = `<img src="dices/dice2.png" alt="dice 2" />`
+  } else if (dados[0] === 3) {
+    dice1El.innerHTML = `<img src="dices/dice3.png" alt="dice 3" />`
+  } else if (dados[0] === 4) {
+    dice1El.innerHTML = `<img src="dices/dice-4.svg" alt="dice 4" />`
+  } else if (dados[0] === 5) {
+    dice1El.innerHTML = `<img src="dices/dice5.png" alt="dice 5" />`
+  } else {
+    dice1El.innerHTML = `<img src="dices/dice-6.png" alt="dice 6" />`
+  }
+  //---------------------------------------------------------------------
+  if (dados[1] === 1) {
+    dice2El.innerHTML = `<img src="dices/dice1.png" alt="dice 1" />`
+  } else if (dados[1] === 2) {
+    dice2El.innerHTML = `<img src="dices/dice2.png" alt="dice 2" />`
+  } else if (dados[1] === 3) {
+    dice2El.innerHTML = `<img src="dices/dice3.png" alt="dice 3" />`
+  } else if (dados[1] === 4) {
+    dice2El.innerHTML = `<img src="dices/dice-4.svg" alt="dice 4" />`
+  } else if (dados[1] === 5) {
+    dice2El.innerHTML = `<img src="dices/dice5.png" alt="dice 5" />`
+  } else {
+    dice2El.innerHTML = `<img src="dices/dice-6.png" alt="dice 6" />`
+  }
+
 }
+
+
 
 function tiradaRandom() {
   let randomNumber = Math.floor(Math.random() * 6) + 1;
